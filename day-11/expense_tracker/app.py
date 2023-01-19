60c6f88138d8c9b853b2a5df6d2a0603acdda14d
Souvik Mondal <souvikmondal2120@gmail.com>
2023-01-19 21:12:07 +0530

    Day 13

--- day-11/expense_tracker/app.py
+++ day-11/expense_tracker/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
-from uuid import uuid1,uuid4
-import os,json,pytz
-from datetime import date,datetime
+from flask import Flask, request, jsonify
+from uuid import uuid1, uuid4
+import os
+import json
+import pytz
+from datetime import date, datetime
+import pandas as pd
 
 db = {}
 db_filename = "db.json"
@@ -9,28 +12,31 @@ db_filename = "db.json"
 # Check whether db.json exists in the directory or not
 if os.path.exists(db_filename):
     print("db exists")
-    with open(db_filename,'r') as f:
+    with open(db_filename, 'r') as f:
         db = json.load(f)
 else:
     print("db does not exists")
     accessKey = str(uuid1())
     secretKey = str(uuid4())
 
-    item_type = ["Food","Beverages","Clothing","Stationaries","Wearables","Electronics Accessories"]
+    item_type = ["Food", "Beverages", "Clothing",
+                 "Stationaries", "Wearables", "Electronics Accessories"]
 
     db = {
         "accessKey": accessKey,
         "secretKey": secretKey,
         "item_type": item_type,
-        "users":[]
+        "users": []
     }
-    with open(db_filename,"w+") as f:
-        json.dump(db,f,indent=4)
+    with open(db_filename, "w+") as f:
+        json.dump(db, f, indent=4)
 
 app = Flask(__name__)
 
 # User sign up
-@app.route('/signup',methods=['POST'])
+
+
+@app.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
         name = request.form['name']
@@ -46,44 +52,48 @@ def signup():
             "purchases": {}
         }
 
-        emailList =[]
+        emailList = []
         for email in db["users"]:
             emailList.append(email["email"])
-        # print(emailList)  
+        # print(emailList)
 
         if len(db["users"]) == 0 or userDict["email"] not in emailList:
             db["users"].append(userDict)
-            with open(db_filename,"r+") as f:
+            with open(db_filename, "r+") as f:
                 f.seek(0)
-                json.dump(db,f,indent=4)
-                
+                json.dump(db, f, indent=4)
+
             return "User added successfully"
         else:
-            return "User already exists"    
-    return "Error: Trying to access endpoint with wrong method"    
+            return "User already exists"
+    return "Error: Trying to access endpoint with wrong method"
 
 # User login
-@app.route('/login',methods=['POST'])
+
+
+@app.route('/login', methods=['POST'])
 def login():
     email = request.form['email']
     password = request.form['password']
-    
+
     userIndex = None
     for user in db["users"]:
         if user["email"] == email and user["password"] == password:
             userIndex = db["users"].index(user)
 
             response = {
-                "message" : "Logged in successfully",
+                "message": "Logged in successfully",
                 "user_index": userIndex
             }
             return response
         else:
             continue
-    return "Wrong email or password!"           
+    return "Wrong email or password!"
 
 # Add purchase
-@app.route('/add_purchase',methods=['POST'])
+
+
+@app.route('/add_purchase', methods=['POST'])
 def add_purchase():
     if request.method == 'POST':
         user_index = int(request.form["user_index"])
@@ -95,10 +105,10 @@ def add_purchase():
         curr_time = str(datetime.now(pytz.timezone("Asia/Kolkata")))
 
         itemDict = {
-            "item_name":item_name,
-            "item_type":item_type,
-            "item_price":item_price,
-            "purchase_time":curr_time
+            "item_name": item_name,
+            "item_type": item_type,
+            "item_price": item_price,
+            "purchase_time": curr_time
         }
 
         existing_dates = list(db["users"][user_index]["purchases"].keys())
@@ -106,19 +116,55 @@ def add_purchase():
             # if there are no purchases user has done for the day
             db["users"][user_index]["purchases"][curr_date] = []
             db["users"][user_index]["purchases"][curr_date].append(itemDict)
-            with open(db_filename,"r+") as f:
+            with open(db_filename, "r+") as f:
                 f.seek(0)
-                json.dump(db,f,indent=4)
+                json.dump(db, f, indent=4)
             return "Item added successfully"
         else:
             db["users"][user_index]["purchases"][curr_date].append(itemDict)
-            with open(db_filename,"r+") as f:
+            with open(db_filename, "r+") as f:
                 f.seek(0)
-                json.dump(db,f,indent=4)
+                json.dump(db, f, indent=4)
             return "Item added successfully"
-        return "Some error occured"
-        
-    
+
+
+# @app.route('/get_purchases_today', methods=['GET'])
+# def get_purchases_today():
+#     user_idx = request.args["user_index"]
+#     # print("user_index: ",user_idx)
+#     curr_date = str(date.today())
+#     list_of_purchases = db["users"][user_idx]["purchases"][curr_date]
+
+#     return jsonify(list_of_purchases)
+
+@app.route("/get_all_purchases_for_today", methods=["GET"])
+def get_all_purchases_for_today():
+    user_idx = int(request.args["user_index"])
+    curr_date = str(date.today())
+    purchases_today = db["users"][user_idx]["purchases"][curr_date]
+    purchasesDates = list(db["users"][user_idx]["purchases"].keys())
+    if (curr_date in purchasesDates):
+        return jsonify(purchases_for_today=purchases_today)
+    else:
+        return jsonify(msg="Data not found")
+
+
+@app.route("/get_purchases", methods=["GET"])
+def get_purchases():
+    data = request.json
+    user_index = data["user_index"]
+    start_date = data["start_date"]
+    end_date = data["end_date"]
+
+    dates = pd.date_range(start_date, end_date)
+    dates_in_db = list(db["users"][user_index]["purchases"].keys())
+    purchaseDict = {}
+    for dt in dates_in_db:
+        if dt in dates:
+            purchaseDict[dt] = db["users"][user_index]["purchases"][dt]
+        else:
+            continue
+    return purchaseDict
 
 
 if __name__ == "__main__":

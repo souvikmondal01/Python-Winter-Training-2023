14f171f20e7243d40bc0366103c8cdcafa7e3d6d
Souvik Mondal <souvikmondal2120@gmail.com>
2023-01-26 19:06:25 +0530

    Day 17

--- day-15/ResumeApp/app.py
+++ day-15/ResumeApp/app.py
@@ -78,14 +78,41 @@ def create_app():
             username = request.args.get('username')
             user = User.query.filter_by(username=username).first()
 
-            experiences_details = request.get_json()   
+            experiences_data = request.get_json()
+            print(experiences_data)
+            for experience in experiences_data["data"]:
+                new_experience = Experiences(
+                    company_name = experience["company_name"],
+                    role = experience["role"],
+                    role_desc = experience["role_desc"],
+                    start_date = experience["start_date"],
+                    end_date = experience["end_date"],
+                    user_id = user.id
+                )
+                db.session.add(new_experience)
+            db.session.commit()
+
+            return jsonify(msg="Experiences Added Successfully")
+   
 
         @app.route("/add_projects_details", methods=["POST"])
         def add_projects_details():
             username = request.args.get('username')
             user = User.query.filter_by(username=username).first()
+            project_data = request.get_json()  
+            print(project_data)
+            for project in project_data["data"]:
+                new_project = Projects(
+                    name = project["name"],
+                    desc = project["description"],
+                    start_date = project["start_date"],
+                    end_date = project["end_date"],
+                    user_id = user.id
+                )
+                db.session.add(new_project)
+            db.session.commit()
 
-            projects_details = request.get_json()  
+            return jsonify(msg="Project Added Successfully")
 
         @app.route("/add_education_details", methods=["POST"])
         def add_education_details():
@@ -106,9 +133,61 @@ def create_app():
             username = request.args.get('username')
             user = User.query.filter_by(username=username).first()
 
-            skills_details = request.get_json()           
+            skills_details = request.get_json()     
+
+        @app.route("/get_resume", methods=["GET"])
+        def get_resume():
+            username = request.args.get('username')
+            user = User.query.filter_by(username=username).first()
+            personal_details = PersonalDetails.query.filter_by(user_id=user.id).first()
+            experiences = Experiences.query.filter_by(user_id=user.id).all()
+            education = Education.query.filter_by(user_id=user.id).all()
+            projects = Projects.query.filter_by(user_id=user.id).all()
+            certificates = Certificates.query.filter_by(user_id=user.id).all()
+            skills = Skills.query.filter_by(user_id=user.id).all()
 
 
+            experiences_data = [] 
+            education_data = [] 
+            projects_data = [] 
+            certificates_data = [] 
+            skills_data = [] 
+
+            resume_data = {
+                "name": personal_details.name,
+                "email": personal_details.email,
+                "phone": personal_details.phone,
+                "address": personal_details.address,
+                "linkedin_link": personal_details.linkedin_link
+            }
+
+            # add experience
+            for exp in experiences:
+                experiences_data.append(
+                    {
+                        "company_name": exp.company_name,
+                        "role": exp.role,
+                        "role_desc": exp.role_desc,
+                        "start_date": exp.start_date,
+                        "end_date": exp.end_date
+                    }
+                )
+            resume_data["experiences"] = experiences_data
+
+            # add projects
+            # for proj in projects:
+            #     projects_data.append(
+            #         {
+            #             "name": proj.name,
+            #             "desc": proj.role,
+            #             "start_date": proj.start_date,
+            #             "end_date": proj.end_date
+            #         }
+            #     )    
+            # resume_data["projects"] = projects_data
+
+            return resume_data
+                   
         # db.drop_all()
         db.create_all()
         db.session.commit()
